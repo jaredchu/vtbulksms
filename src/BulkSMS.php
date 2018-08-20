82065a72ac10bf82fc875e9281efe6b8592f4849
Jared Chu <me@jaredchu.com>
2018-08-20 22:41:02 +0700

    Create directory structure

---
+++ src/BulkSMS.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jaredchu
+ * Date: 20/08/2018
+ * Time: 22:30
+ */
+
+namespace JC\Viettel\WebService;
+
+
+class BulkSMS
+{
+
+}
---
+++ tests/BulkSMSTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jaredchu
+ * Date: 20/08/2018
+ * Time: 22:33
+ */
+
+class BulkSMSTest extends PHPUnit_Framework_TestCase
+{
+    public function testInit(){
+        $this->assertTrue(true);
+    }
+}
